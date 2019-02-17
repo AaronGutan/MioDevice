ad282edd7cda846f48458619f1e8c57afedcd007
AaronGutan <andreykaq@gmail.com>
2019-02-17 19:37:11 +0500

    Add files via upload

---
+++ Mio_device.ino
@@ -0,0 +1,47 @@
+// Видеообзоры и уроки работы с ARDUINO на YouTube-канале IOMOIO: https://www.youtube.com/channel/UCmNXABaTjX_iKH28TTJpiqA
+
+#include <OLED_I2C.h>                               // Подключаем библиотеку OLED_I2C для работы со шрифтами и графикой
+#include <HX711.h>                                  // Подключаем библиотеку HX711 для работы с АЦП hx711
+
+HX711 scale(A1, A0);                                // Указываем в какие пины подключен АЦП hx711 (DT(DOUT)  - pin A1, SCK - pin A0)
+OLED  myOLED(A4, A5, A4);                           // Определяем пины OLED дисплея: SDA - пин A4, SCL - пин A5
+extern uint8_t RusFont[];                           // Подключаем русский шрифт
+extern uint8_t BigNumbers[];                        // Подключаем цифровой шрифт
+
+float myScale;                                      // Инициализируем переменную в которой будем хранить текущий вес
+
+void setup() {
+  myOLED.begin();                                   // Инициализируем дисплей, как устройство вывода
+  myOLED.clrScr();                                  // Стираем все с экрана
+  myOLED.setFont(RusFont);                          // Инициализируем русский шрифт
+  myOLED.print("Initialization.", LEFT, 0);         // Выводим надпись "Идет калибровка"
+  myOLED.update();                                  // Обновляем содержимое экрана
+  
+  scale.set_scale(3333.33);                          // Устанавливаем калибровочный коэффициент 
+  scale.tare();                                     // Сбрасываем весы на 0
+  myScale = round(scale.get_units(10));             // Запоминаем среднее значение без груза, но с учетом тары 
+  
+  myOLED.clrScr();                                  // Стираем все с экрана
+  myOLED.setFont(BigNumbers);                       // Инициализируем шрифт BigNumbers
+  myOLED.print("0.00", CENTER, 20);                 // Обнуляем значение веса на экране
+  myOLED.update();                                  // Выводим на дисплей
+}
+
+void loop() {
+  scale.power_up();                                 // Отключаем энергосберегающий режим hx711
+  if (round(scale.get_units(10)) != myScale) {      // Условие выполняется, если округленный до граммов вес отличается от последнего
+    myScale = scale.get_units(20);                  // Запоминаем текущий вес
+    myOLED.clrScr();                                // Стираем все с экрана
+    myOLED.setFont(BigNumbers);                     // Инициализируем шрифт вывода веса
+   
+    if (myScale > 0.5)                              // Если вес не ничтожно мал
+      myOLED.print(String(myScale), CENTER, 20);    // то выводим его
+    else                                            // иначе
+      myOLED.print("0.00", CENTER, 20);             // выводим нули
+   
+    myOLED.update();                                // Обновляем информацию на дисплее
+    myScale = round(myScale);                       // Запоминаем округленный текущий вес (для сравнения)
+  }
+  scale.power_down();                               // Включаем энергосберегающий режим hx711
+  delay(300);                                       // Пауза на половину сек.
+}
---
+++ iomoio_hx711_calibration.ino
@@ -0,0 +1,57 @@
+// Видеообзоры и уроки работы с ARDUINO на YouTube-канале IOMOIO: https://www.youtube.com/channel/UCmNXABaTjX_iKH28TTJpiqA
+
+#include <OLED_I2C.h>
+#include "HX711.h"
+// Скетч выводит в монитoр порта Arduino IDE калибровочный коэффициент весов для функции set_scale()
+// Впишите известный вам вес предмета в граммах в строку с указывающим комментарием в нижней части этого скетча (рекомендуется от 500 до 1000 граммов)
+// Загрузите скетч в Arduino
+// Убедитесь, что на весах нет груза
+// Запустите монитор порта
+// В течение 5-и секунд после появления надписи Ready поместите на весы предмет, вес которого вам известен
+// Запомните значение set_scale - это ваш калибровочный коэффициент
+
+// HX711.DOUT  - pin #A1
+// HX711.PD_SCK - pin #A0
+HX711 scale(A1, A0);
+float myScale;
+float myRatio;
+
+OLED  myOLED(A4, A5, A4);                           // Определяем пины OLED дисплея: SDA - пин A4, SCL - пин A5
+extern uint8_t RusFont[];                           // Подключаем русский шрифт
+extern uint8_t BigNumbers[];
+
+void setup() {
+
+  myOLED.begin();                                   // Инициализируем дисплей, как устройство вывода
+  myOLED.clrScr();                                  // Стираем все с экрана
+  myOLED.setFont(RusFont);                          // Инициализируем русский шрифт
+  myOLED.print("calibration...", LEFT, 0);         // Выводим надпись "Идет калибровка"
+  myOLED.update();
+  
+  //Serial.begin(9600);
+  scale.set_scale();
+  scale.tare();
+  //Serial.println("Ready");
+  
+  myOLED.clrScr();                                  // Стираем все с экрана
+  myOLED.setFont(RusFont);                          // Инициализируем русский шрифт
+  myOLED.print("Ready", LEFT, 0);         // Выводим надпись "Идет калибровка"
+  myOLED.update();
+  
+  delay(5000);
+  myScale = scale.get_units(10);
+  myRatio = myScale/82;          // В этой строке необходимо заменить число на известный вам вес в граммах
+  //Serial.print("set_scale: ");
+  //Serial.println(myRatio);
+
+  myOLED.clrScr();                                  // Стираем все с экрана
+  myOLED.setFont(BigNumbers);                       // Инициализируем шрифт BigNumbers
+  myOLED.print(String(myRatio), CENTER, 20);                 // Обнуляем значение веса на экране
+  myOLED.update();
+  
+  
+}
+
+void loop() {
+
+}
