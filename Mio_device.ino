47fecdb2a872addfec147860fa8f292dd475f304
AaronGutan <andreykaq@gmail.com>
2019-08-15 11:29:45 +0500

    Add files via upload
    Add mapping

--- Mio_device.ino
+++ Mio_device.ino
@@ -13,51 +13,59 @@ extern uint8_t RusFont[];                           // Подключаем ру
 extern uint8_t BigNumbers[];                        // Подключаем цифровой шрифт
 
 int   myScale;                                      // Инициализируем переменную в которой будем хранить текущий вес
+
 float digit;
 
 void setup() {
   myOLED.begin();                                   // Инициализируем дисплей, как устройство вывода
   myOLED.clrScr();                                  // Стираем все с экрана
   myOLED.setFont(BigNumbers);                          // Инициализируем русский шрифт
-  myOLED.print("Initialization.", LEFT, 0);         // Выводим надпись "Идет калибровка"
+  myOLED.print("Initialization.", CENTER, 0);         // Выводим надпись "Идет калибровка"
   myOLED.update();                                  // Обновляем содержимое экрана
 
-  scale.set_scale(3333.33);                          // Устанавливаем калибровочный коэффициент
+  scale.set_scale(3331.33);                          // Устанавливаем калибровочный коэффициент
   scale.tare();                                     // Сбрасываем весы на 0
   myScale = round(scale.get_units(10));             // Запоминаем среднее значение без груза, но с учетом тары
+  
 
   myOLED.clrScr();                                  // Стираем все с экрана
   myOLED.setFont(BigNumbers);                       // Инициализируем шрифт BigNumbers
-  myOLED.print("000", CENTER, 20);                 // Обнуляем значение веса на экране
+  myOLED.print("00", CENTER, 20);                 // Обнуляем значение веса на экране
   myOLED.update();                                  // Выводим на дисплей
 
   pinMode(buttonPin, INPUT);
 }
 
 void loop() {
+
+//Serial.print(myScale);
   
   buttonState = digitalRead(buttonPin);
   
   if (buttonState == HIGH) {
 
     scale.power_up();                                 // Отключаем энергосберегающий режим hx711
-    //if (round(scale.get_units(10)) != myScale) {      // Условие выполняется, если округленный до граммов вес отличается от последнего
+
       myScale = scale.get_units(10);                  // Запоминаем текущий вес
       myOLED.clrScr();                                // Стираем все с экрана
-      //myOLED.setFont(BigNumbers);                     // Инициализируем шрифт вывода веса
 
       digit = round(myScale/10);
       myScale = int(digit * 10);
 
-      if (myScale > 50)                              // Если вес не ничтожно мал
-        myOLED.printNumI(myScale, CENTER, 20, 3);      // то выводим его
-      else                                            // иначе
-        myOLED.print("000", CENTER, 20);             // выводим нули
+      
+        if (myScale > 20)                              // Если вес не ничтожно мал
+          myOLED.printNumI(map(myScale,0,1800,0,100), CENTER, 20);      // то выводим его
+        else                                            // иначе
+          myOLED.print("00", CENTER, 20);             // выводим нули
+        
+          
+      }
 
       myOLED.update();                                // Обновляем информацию на дисплее
-      myScale = round(myScale);                       // Запоминаем округленный текущий вес (для сравнения)
-    //}
+      //myScale = round(myScale);                       // Запоминаем округленный текущий вес (для сравнения)
+      
+
     scale.power_down();                               // Включаем энергосберегающий режим hx711
     //delay(300);                                       // Пауза на половину сек.
-  }
+  
 }
